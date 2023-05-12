e1ca8a1961b9e4b16283c470c14c2d874cc3bc8c
Liz <claireliang2012@gmail.com>
2023-05-12 11:56:03 +0800

    Finish iteration 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 // Styling
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
-// Data
+// Countries data from JSON
 import countries from "./countries.json";
 
 // Router
@@ -16,17 +17,28 @@ import CountriesList from "./components/CountriesList";
 import CountryDetails from "./components/CountryDetails";
 
 function App() {
+  const [countriesDataFromApi, setCountriesDataFromApi] = useState();
+
+  useEffect(() => {
+    // Countries data from API
+    fetch("https://ih-countries-api.herokuapp.com/countries", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => setCountriesDataFromApi(data));
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
       <div className="container">
         <div className="row">
-          <CountriesList countries={countries} />
+          <CountriesList countries={countriesDataFromApi} />
           <Routes>
             <Route index />
             <Route
               path="/:countryId"
-              element={<CountryDetails countries={countries} />}
+              element={<CountryDetails countries={countriesDataFromApi} />}
             />
             <Route path="*" element={<h1>404 Not Found</h1>} />
           </Routes>
